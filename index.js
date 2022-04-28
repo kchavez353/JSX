7b2ba80558df6a2c315aa9857b73b6376482a4af
Keavagh Chavez <80922170+kchavez353@users.noreply.github.com>
2022-04-28 11:20:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My favorite foods! </h1>
+    <ul>
+      <li>Pizza</li>
+      <li>Burgers</li>
+      <li>Nachos</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
